cc2a0d67c1af15cb4859fa24b4243b4ed2592061
Wakidou <wakidou.abdallah@gmail.com>
2025-04-07 18:12:19 +0200

    added config for github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: '/3D-solar-system-nuxt-three.js/',
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
